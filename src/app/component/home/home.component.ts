47611e6774ee27d1d58d235d5d7f328e139272a6
elouissi <yassinelouissi67@gmai.com>
2025-02-04 16:49:51 +0100

    finish the authentication

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -8,6 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  private recyclingBackgrounds = [
+    'https://images.unsplash.com/photo-1617358141206-1d675bf8a2a7', // Tri des déchets
+    'https://images.unsplash.com/photo-1591193686104-c28cef0d9c4f', // Recyclage de plastique
+    'https://images.unsplash.com/photo-1587473472943-c80f4f44c4c0', // Poubelles de recyclage
+    'https://images.unsplash.com/photo-1507346580942-3c5bc8de6520'  // Centre de recyclage
+  ];
+
+  getRandomRecyclingBackground(): string {
+    const randomIndex = Math.floor(Math.random() * this.recyclingBackgrounds.length);
+    return this.recyclingBackgrounds[randomIndex];
+  }
 
 
 }
